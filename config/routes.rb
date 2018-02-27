b4536cf204988d9267890396ff7bdb9754879133
albCh <alberto.chang.lopez@gmail.com>
2018-02-27 07:41:13 -0500

    config system parameters route

--- config/routes.rb
+++ config/routes.rb
@@ -36,8 +36,7 @@ Rails.application.routes.draw do
     put "toggle_status", on: :member
   end
 
-  resources :system_parameters do
-  end
+  resources :system_parameters
 
   resources :audits, only: [:index]
 end

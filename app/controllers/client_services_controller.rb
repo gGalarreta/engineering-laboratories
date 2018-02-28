20039a0915fb34ec6c7559394cf5e4ad6419a554
gustavo Galarreta <gustavo.galarrea@pucp.pe>
2018-02-28 17:49:36 -0500

    services with advanced search

--- app/controllers/client_services_controller.rb
+++ app/controllers/client_services_controller.rb
@@ -23,6 +23,12 @@ class ClientServicesController < ApplicationController
   def show
   end
 
+  def search
+    start_date = params[:start_date].to_date.strftime("%m/%d/%Y")
+    end_date = params[:end_date].to_date.strftime("%m/%d/%Y")
+    @services = Service.advanced_search(start_date, end_date)
+  end
+
   private
     def laboratories
       @laboratories = Laboratory.only_actives
--- app/controllers/employee_services_controller.rb
+++ app/controllers/employee_services_controller.rb
@@ -9,6 +9,12 @@ class EmployeeServicesController < ApplicationController
   def show
   end
 
+  def search
+    start_date = params[:start_date].to_date.strftime("%m/%d/%Y")
+    end_date = params[:end_date].to_date.strftime("%m/%d/%Y")
+    @services = Service.advanced_search(start_date, end_date)
+  end  
+
   private
     def set_service
       @service = Service.find params[:id]
